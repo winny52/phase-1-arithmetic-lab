de7fd36a24a9e9fc6928f205a9663647165f2344
WinnySigilai <winnysigilai02@gmail.com>
2023-06-21 09:37:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 *num2; 
+
+let random = Math.floor (Math.random()*10);
+console.log(random);
+
+const num3 = 40
+const num4 = 9
+
+const mod = num3 % num4 ;
+
+const set = [5, 10, 15, 20, 8, 3, 12]; 
+const max = Math.max(...set);
+
+console.log(multiply);
+console.log(max);
+console.log(mod);
